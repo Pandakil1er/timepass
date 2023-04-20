090adeb4a78096f24cd2d88151d5a886f84d2cdf
Pandakil1er <Anshajsingh61@gmail.com>
2023-04-21 01:32:43 +0530

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".button");
+
+btn.addEventListener("click", () => {
+  console.log("working");
+  document.body.style.backgroundColor = "red";
+  btn.textContent = "Love You Bitch";
+  head.style.color = "black";
+});
