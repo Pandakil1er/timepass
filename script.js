bd5ca650f6b7ac608d11f11d0b6318aa181610f8
Pandakil1er <Anshajsingh61@gmail.com>
2023-04-21 01:42:52 +0530

    name

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const btn = document.querySelector(".button");
 btn.addEventListener("click", () => {
   console.log("working");
   document.body.style.backgroundColor = "red";
-  btn.textContent = "Love You Bitch";
+  btn.textContent = "Love You Himanshi baby";
   head.style.color = "black";
 });
