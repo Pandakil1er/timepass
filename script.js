db89eb5e417eeb5356adf82bd0cf98f6eca49097
Pandakil1er <120533282+Pandakil1er@users.noreply.github.com>
2023-05-05 05:42:24 +0530

    Sweet!?

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const btn = document.querySelector(".button");
 btn.addEventListener("click", () => {
   console.log("working");
   document.body.style.backgroundColor = "red";
-  btn.textContent = "Love You baby";
+  btn.textContent = "Love You Sweetu";
   head.style.color = "black";
 });
